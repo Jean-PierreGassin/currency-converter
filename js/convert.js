ffa39b2771cd12b884e58686b6b9f8e5ae997659
Jean-Pierre <jeanpierre.gassin@gmail.com>
2016-05-12 11:05:26 +1000

    removal of redundant files

--- js/convert.js
+++ js/convert.js
@@ -3,14 +3,14 @@ function getRate(from, to) {
     script.setAttribute('src', "http://query.yahooapis.com/v1/public/yql?q=select%20rate%2Cname%20from%20csv%20where%20url%3D'http%3A%2F%2Fdownload.finance.yahoo.com%2Fd%2Fquotes%3Fs%3D"+from+to+"%253DX%26f%3Dl1n'%20and%20columns%3D'rate%2Cname'&format=json&callback=parseExchangeRate");
     document.body.appendChild(script);
 }
- 
+
 function parseExchangeRate(data) {
     var amount = document.getElementById('amount').value;
     var exchangeBox = document.getElementById('rateBox');
     var name = data.query.results.row.name;
     var rate = parseFloat(data.query.results.row.rate, 10);
     var convert = amount*rate;
-    var convert = convert.toFixed(2);
+    convert = convert.toFixed(2);
 
     document.getElementById('convertedBox').value = "= " + convert.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     exchangeBox.innerHTML = "The exchange rate from <b>" + name + "</b>, is <i>" + rate + "</i>.";
@@ -21,4 +21,3 @@ function convert() {
     var to = $('#to option:selected').val();
     getRate(from, to);
 }
-
